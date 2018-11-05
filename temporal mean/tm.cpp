b8c6bb9e15b2e80b6e08d7ad085ee06545ee8476
zer01ike <wangkai_like@163.com>
2018-11-05 21:14:21 +0800

    temporal mean complete

--- temporal mean/tm.cpp
+++ temporal mean/tm.cpp
@@ -15,5 +15,32 @@ int main(){
     vector<cv::Mat> video = tool.readVideo(path);
 
 
+    int rows = video[0].rows;
+    int cols = video[0].cols;
+    double frames = video.size();
+    cout<<rows<<" "<<cols<<endl;
+    cv::Mat reference = cv::Mat(cv::Size(cols,rows),CV_64FC3,cv::Scalar(0.0f,0.0f,0.0f));
+    cv::Mat count = cv::Mat(cv::Size(cols,rows),CV_64FC3,cv::Scalar(frames,frames,frames));
+    cv::namedWindow("result");
+
+    vector<cv::Mat>::iterator frame = video.begin();
+
+    for(;frame != video.end();++frame){
+        //count ++;
+        cv::Mat temp = *frame;
+        temp.convertTo(temp,CV_64FC3);
+        reference +=temp;
+    }
+
+    reference = reference/count;
+
+    cv::Mat result;
+    reference.convertTo(result,CV_8UC3);
+
+    cv::imshow("result",result);
+    cv::waitKey();
+    cv::destroyAllWindows();
+
+
     return 0;
 }
